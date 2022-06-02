78f1b5f06f00f69c330c5005c9e02eff4faf965e
Paurav Shah <pauravshah1999@gmail.com>
2022-06-02 16:34:58 +0530

    just another change

--- src/main/resources/employee_record.sql
+++ src/main/resources/employee_record.sql
@@ -1,14 +1,13 @@
 CREATE DATABASE employee_record;
 USE employee_record;
 CREATE TABLE employee (
-    emp_id INT(3) NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    gender VARCHAR(255) NOT NULL,
-    dob DATE NOT NULL,
-    age INT(3) NOT NULL,
-    salary DECIMAL(7,2) NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    PRIMARY KEY (emp_id)
+    emp_id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255),
+    gender VARCHAR(255),
+    dob VARCHAR(255),
+    age INT(3),
+    salary DECIMAL(10,2),
+    status VARCHAR(255)
 );
